9a1ed694727ac74ad93b2337e77022640b7dc356
Kumar Ritush <72932110+Ace-07@users.noreply.github.com>
2025-05-02 12:08:32 +0530

    Create 6.java

---
+++ Rough work/dsa practice/stars - 22 pattern/6.java
@@ -0,0 +1,47 @@
+import java.lang.reflect.Array;
+import java.util.*;
+import java.lang.Math;
+import java.util.Arrays;
+import java.util.Collections;
+
+//permutation
+//possibility
+//formula
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        scanner.nextLine();
+        for (int i = 0; i<n;i++){
+
+            for (int j = 1;j<(n+1)-i;j++){
+                System.out.print(j);
+
+            }
+            System.out.println();
+
+
+
+            }
+        }
+    }
+
+/*
+5
+12345
+1234
+123
+12
+1
+*/
+
+
+
+
+
+
+
