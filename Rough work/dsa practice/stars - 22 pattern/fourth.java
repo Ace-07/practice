5dc14823171a4867d66c2a68fc160348ee7d884a
Kumar Ritush <72932110+Ace-07@users.noreply.github.com>
2025-05-02 11:58:04 +0530

    Create fourth.java

---
+++ Rough work/dsa practice/stars - 22 pattern/fourth.java
@@ -0,0 +1,45 @@
+import java.lang.reflect.Array;
+import java.util.*;
+import java.lang.Math;
+import java.util.Arrays;
+import java.util.Collections;
+
+//permutation
+//possibility
+//formula
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.nextLine();
+        for (int i = 0; i<n;i++){
+
+            for (int j = 1;j<i+2;j++){
+                System.out.print(i+1);
+
+            }
+            System.out.println();
+
+
+
+            }
+        }
+    }
+/*
+6
+1
+22
+333
+4444
+55555
+666666
+*/
+
+
+
+
+
+
