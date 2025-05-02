afec06f75f70aa95610b9f6ad249d825447374bf
Kumar Ritush <72932110+Ace-07@users.noreply.github.com>
2025-05-02 11:49:52 +0530

    Create third.java

---
+++ Rough work/dsa practice/stars - 22 pattern/third.java
@@ -0,0 +1,38 @@
+import java.lang.reflect.Array;
+import java.util.*;
+import java.lang.Math;
+import java.util.Arrays;
+import java.util.Collections;
+
+//permutation
+//possibility
+//formula
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.nextLine();
+        for (int i = 0; i<n;i++){
+
+            for (int j = 1;j<i+2;j++){
+                System.out.print(j);
+
+            }
+            System.out.println();
+
+
+
+            }
+        }
+    }
+
+/*
+4
+1
+12
+123
+1234
+*/
