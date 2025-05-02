38bc21c76a02c0741aa01e8db204b9fc2ce888c5
Kumar Ritush <72932110+Ace-07@users.noreply.github.com>
2025-05-02 12:04:54 +0530

    Create 5.java

---
+++ Rough work/dsa practice/stars - 22 pattern/5.java
@@ -0,0 +1,47 @@
+import java.lang.reflect.Array;
+import java.util.*;
+import java.lang.Math;
+import java.util.Arrays;
+import java.util.Collections;
+
+//permutation
+//possibility
+//formula
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        char ch = '*';
+        scanner.nextLine();
+        for (int i = 0; i<n;i++){
+
+            for (int j = 0;j<n-i;j++){
+                System.out.print(ch);
+
+            }
+            System.out.println();
+
+
+
+            }
+        }
+    }
+/*
+5
+*****
+****
+***
+**
+*
+
+*/
+
+
+
+
+
+
+
