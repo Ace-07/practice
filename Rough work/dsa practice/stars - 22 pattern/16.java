b49deb7f131daac2ff6871cceee49d6379be99f3
Kumar Ritush <72932110+Ace-07@users.noreply.github.com>
2025-05-05 12:03:21 +0530

    Create 16.java

---
+++ Rough work/dsa practice/stars - 22 pattern/16.java
@@ -0,0 +1,142 @@
+import java.lang.reflect.Array;
+import java.util.*;
+import java.lang.Math;
+import java.util.Arrays;
+import java.util.Collections;
+
+//permutation
+//possibility
+//formula
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+
+
+
+        char ch = 'A';
+
+
+
+        scanner.nextLine();
+        for (int i = 0; i < n; i++) {
+
+            for (int j =0;j<i+1;j++){
+                System.out.print(ch);
+            }
+            System.out.println();
+            ch++;
+
+
+
+
+            }
+
+
+
+        }
+//        for (int k = 0 ;k<n;k++){
+//            for(int m = 0;m<n-(k+1);m++){
+//                System.out.print(" ");
+//
+//            }
+//            for(int f = 0; f<k+1;f++){
+//                System.out.print(n-(n-(f+1)));
+//            }
+//            System.out.println();
+//
+//        }
+    }
+
+// AND****************
+
+import java.lang.reflect.Array;
+import java.util.*;
+import java.lang.Math;
+import java.util.Arrays;
+import java.util.Collections;
+
+//permutation
+//possibility
+//formula
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+
+
+
+        char ch = 'A';
+
+
+
+        scanner.nextLine();
+        for (int i = 0; i < n; i++) {
+
+            for (int j =0;j<i+1;j++){
+                System.out.print((char)(ch + i));
+            }
+            System.out.println();
+
+
+
+
+
+            }
+
+
+
+        }
+//        for (int k = 0 ;k<n;k++){
+//            for(int m = 0;m<n-(k+1);m++){
+//                System.out.print(" ");
+//
+//            }
+//            for(int f = 0; f<k+1;f++){
+//                System.out.print(n-(n-(f+1)));
+//            }
+//            System.out.println();
+//
+//        }
+    }
+/*
+5
+A
+BB
+CCC
+DDDD
+EEEEE
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
