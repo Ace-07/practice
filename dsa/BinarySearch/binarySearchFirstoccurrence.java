d1dc16a6c65bd8b9f62c97101af4a5cfa4e90789
Kumar Ritush <72932110+Ace-07@users.noreply.github.com>
2025-08-24 03:18:07 +0530

    Create binarySearchFirstoccurrence.java

---
+++ dsa/BinarySearch/binarySearchFirstoccurrence.java
@@ -0,0 +1,47 @@
+
+
+import java.lang.reflect.Array;
+import java.util.*;
+import java.lang.Math;
+import java.util.Arrays;
+import java.util.Collections;
+
+//permutation
+//possibility
+//formula
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+    public class Main {
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int[] A = {1,2,4,4,4,5,6,7};
+            int key = 2;
+            int n = A.length;
+
+            System.out.println(BinarySearch(A,n,key));
+        }
+        public static int BinarySearch(int[]A, int n , int key){
+            int low = 0;
+            int high = n-1;
+            int result = -1;
+            while(low<=high){
+                int mid = low + (high - low)/2;
+                if (A[mid]==key){
+                    result= mid;//if the element if found it will search the low side till we get the first occurance
+                    high= mid-1;// update the condition
+                } else if (A[mid]>key) {
+                    high = mid - 1;
+                }
+                else
+                    low = mid+1;
+            }
+            return result;
+        }
+    }
+/*
+2
+*/
+
+
