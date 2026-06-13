fe2931c24599eba0ce96be4b30767fe0e2ffa269
Kumar Ritush <72932110+Ace-07@users.noreply.github.com>
2026-06-13 18:53:32 +0530

    Create sol.java

---
+++ MountBlue Job Challenge/Sales by Match/sol.java
@@ -0,0 +1,15 @@
+
+public static int sockMerchant(int n, List<Integer> ar) {       
+        Map<Integer,Integer>map = new HashMap<>();
+        for(int num : ar){
+            map.put(num, map.getOrDefault(num, 0)+1);// if the number is there add freq 1
+        }
+        int total = 0;
+        for (int count:map.values()){
+            total = total + count/2;
+        }
+        
+        return total;
+        
+
+    }
